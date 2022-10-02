d57e14f75e3d9fc8c94da7cad1b5009800471981
Petr Lebedev <43229469+defast@users.noreply.github.com>
2022-10-02 20:51:21 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 set -e
 
-
-
 for file in $(ls)
 do
   echo "## Running PHP Syntax Checker (lint) on ${file}"
-if [ -d "$file" ]
+if [ -d "$file" ] && [ "$file" != "bitrix" ]
 then
 phpcs --standard=PSR1,PSR12 $file
-elif if [[ $file == *.txt ]]
+elif [ -f "$file" ] && [[ $file == *.php ]]
 then
 phpcs --standard=PSR1,PSR12 $file
 fi
 done
-
 
 #echo "## Running PHP Syntax Checker (lint) on ${var}"
 #phpcs --standard=PSR1,PSR12
