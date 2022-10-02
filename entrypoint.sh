4bee067ac44101f412cdcbea775e492c43880abd
Petr Lebedev <43229469+defast@users.noreply.github.com>
2022-10-02 20:43:53 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 set -e
 
-echo "## Running PHP Syntax Checker (lint) on ${var}"
-phpcs --standard=PSR1,PSR12
+
+
+for file in $(ls)
+do
+  echo "## Running PHP Syntax Checker (lint) on ${file}"
+if [ -d "$file" ]
+then
+phpcs --standard=PSR1,PSR12 $file
+elif if [[ $file == *.txt ]]
+then
+phpcs --standard=PSR1,PSR12 $file
+fi
+done
+
+
+#echo "## Running PHP Syntax Checker (lint) on ${var}"
+#phpcs --standard=PSR1,PSR12
 
 exit "${?}"
