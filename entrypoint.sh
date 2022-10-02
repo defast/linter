40ffc2a7a4ac0a622cda93f04fea70978c98c086
Petr Lebedev <43229469+defast@users.noreply.github.com>
2022-10-02 14:36:08 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,24 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-phplint --configuration='/.phplint.yml' ./
+if [ -z "$1" ]; then
+  DIR_TO_SCAN="."
+else
+  DIR_TO_SCAN="$1"
+fi
+
+
+if [ ! -d "${DIR_TO_SCAN}" ] && [ ! -f "${DIR_TO_SCAN}" ]; then
+  echo "\nInvalid directory or file: ${DIR_TO_SCAN}"
+  echo "\n\n"
+
+  exit 2
+fi
+
+
+echo "## Running PHP Syntax Checker (lint) on ${var}"
+noverify check --exclude-checks='unused' --allow-checks='arraySyntax' --exclude='bitrix' ${DIR_TO_SCAN}
+
+
+
+exit "${?}"
