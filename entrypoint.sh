32650c81e7d7745f71d561838dcf6ce235ff57fe
Petr Lebedev <43229469+defast@users.noreply.github.com>
2022-10-03 21:50:37 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ else
   IGNORE_DIR="$1"
 fi
 
+echo "phpcs -n --standard=PSR1,PSR12 --ignore=${IGNORE_DIR} --report=full --extensions=php $(ls)"
+
 phpcs -n --standard=PSR1,PSR12 --ignore="${IGNORE_DIR}" --report=full --extensions=php $(ls)
 
 #echo "## Running PHP Syntax Checker (lint) on ${var}"
